4439400f0ed455b7a79be0bc8e2aff6d3e5ac596
fontwan <fontwanhood@gmail.com>
2017-03-27 21:39:04 -0500

    Update event.rb

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,9 +2,9 @@ class Event < ApplicationRecord
   acts_as_paranoid
   validates :event_name, presence: true
   has_many :customer_events
-  has_many :customers, :through => :customer_events
+  has_many :customers, :through => :customer_event
   has_many :employee_events
-  has_many :employees, :through => :employee_events
+  has_many :employees, :through => :employee_event
   belongs_to :color
   belongs_to :event_status
   belongs_to :event_type
