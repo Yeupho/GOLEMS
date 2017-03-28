ec2daaee527ca804113b4d295fe6c575f45de659
fontwan <fontwanhood@gmail.com>
2017-03-27 21:37:20 -0500

    Update product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,6 @@ class Product < ApplicationRecord
   validates :product_price, presence: true
   belongs_to :product_type
   has_many :customer_event_products
-  has_many :customer_events, :through => :customer_event_products
+  has_many :customer_events, :through => :customer_event_product
 
 end
