73f7d6d52221c200471a6c652690b90ddf2214f9
fontwan <fontwanhood@gmail.com>
2017-03-27 21:34:26 -0500

    Update customer_event_product.rb

--- app/models/customer_event_product.rb
+++ app/models/customer_event_product.rb
@@ -1,6 +1,6 @@
 class CustomerEventProduct < ApplicationRecord
   acts_as_paranoid
-
+  validates :quantity, presence: true
   belongs_to :customer_event
   belongs_to :product
   belongs_to :pickup_status
