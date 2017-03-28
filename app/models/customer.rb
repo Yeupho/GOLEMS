2a4bf9c63422f7140f1d1b10af8bdf00fc19e470
fontwan <fontwanhood@gmail.com>
2017-03-27 21:33:24 -0500

    Update customer.rb

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,9 @@
 class Customer < ApplicationRecord
-
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :phone, presence: true
+  validates :zipcode, presence: true
   has_many :customer_events
   has_many :events, :through => :customer_events
   belongs_to :customer_status
