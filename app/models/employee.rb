c9652d5cbcf01206e1bf1ef2ba8b37dcde8684f3
fontwan <fontwanhood@gmail.com>
2017-03-27 21:34:58 -0500

    Update employee.rb

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,6 +1,12 @@
 class Employee < ApplicationRecord
   acts_as_paranoid
-
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :phone, presence: true
+  validates :address, presence: true
+  validates :city, presence: true
+  validates :zipcode, presence: true
   has_many :employee_events
   has_many :employee_type, :through => :positions
   has_many :positions
