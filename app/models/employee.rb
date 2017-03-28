2a987c99d5f100291257bd4fc49c4ca0be69de81
fontwan <fontwanhood@gmail.com>
2017-03-27 21:39:54 -0500

    Update employee.rb

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -8,9 +8,9 @@ class Employee < ApplicationRecord
   validates :city, presence: true
   validates :zipcode, presence: true
   has_many :employee_events
-  has_many :employee_type, :through => :positions
+  has_many :employee_type, :through => :position
   has_many :positions
-  has_many :events, :through => :employee_events
+  has_many :events, :through => :employee_event
   belongs_to :employee_status
   belongs_to :state
   belongs_to :country
