56e1cfeaadd298704ca4210b527d7b727534b5e3
fontwan <fontwanhood@gmail.com>
2017-03-27 21:40:33 -0500

    Update customer_event.rb

--- app/models/customer_event.rb
+++ app/models/customer_event.rb
@@ -7,6 +7,6 @@ class CustomerEvent < ApplicationRecord
   belongs_to :customer
   belongs_to :event
   has_many :customer_event_products
-  has_many :products, :through => :customer_event_products
+  has_many :products, :through => :customer_event_product
 
 end
