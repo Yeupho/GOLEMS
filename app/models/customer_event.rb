acd364387b86b45a7b8bfb5f51791dace3feb5fd
Yeupho <hotaznbuns@gmail.com>
2017-04-02 17:57:33 -0500

    Typo, need to pluralize :through=>

--- app/models/customer_event.rb
+++ app/models/customer_event.rb
@@ -7,7 +7,7 @@ class CustomerEvent < ApplicationRecord
   belongs_to :customer
   belongs_to :event
   has_many :customer_event_products
-  has_many :products, :through => :customer_event_product
+  has_many :products, :through => :customer_event_products
 
   def phone
     customer.try(:phone)
