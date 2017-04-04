d0c40cec633073c5d22cb92286ded6c7a9403025
Yeupho <hotaznbuns@gmail.com>
2017-04-04 16:50:16 -0500

    removed temp issues

--- app/controllers/archive_controller.rb
+++ app/controllers/archive_controller.rb
@@ -8,10 +8,6 @@ class ArchiveController < ApplicationController
     @customer_events = CustomerEvent.all
     @customer_event_products = CustomerEventProduct.all
     @colors = Color.all
-    @EmpDayReport = Employee.find_by_sql('select pv.id, pv.product_name from products pv
-                                          left join customer_event_products cep on pv.id = cep.product_id
-                                          where quantity is null
-                                          order by pv.id')
 
   end
 
