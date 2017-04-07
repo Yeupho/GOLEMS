f2d392a57e276796a4c308029c66e3da10e1882a
Jen0118@ <tranle.steven@gmail.com>
2017-04-07 15:37:53 -0500

    S 4/7

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,10 +1,10 @@
 class AdminController < ApplicationController
   require 'will_paginate/array'
   def index
-    @colors = Color.all.paginate(page: params[:color_page], per_page: 11)
+    @colors = Color.all.paginate(page: params[:color_page], per_page: 10)
     @color = Color.new
-    @products = Product.all.paginate(page: params[:product_page], per_page: 11)
-    @event_types = EventType.all.paginate(page: params[:event_type_page], per_page: 11)
-    @positions = Position.all.paginate(page: params[:position_page], per_page: 11)
+    @products = Product.all.paginate(page: params[:product_page], per_page: 10)
+    @event_types = EventType.all.paginate(page: params[:event_type_page], per_page: 10)
+    @positions = Position.all.paginate(page: params[:position_page], per_page: 10)
   end
 end
