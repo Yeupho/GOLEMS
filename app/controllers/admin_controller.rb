2dd7ed3a9c5980e628f09ca8a81a6f0efd726a17
Luke Nguyen <Pho@Lukes-MBP.attlocal.net>
2017-04-09 02:22:17 -0500

    more changes

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -5,8 +5,11 @@ class AdminController < ApplicationController
     @color = Color.new
     @products = Product.all.paginate(page: params[:product_page], per_page: 11)
     @event_types = EventType.all.paginate(page: params[:event_type_page], per_page: 11)
-    @positions = Position.all.paginate(page: params[:position_page], per_page: 11)
+    @positions = Position.all.paginate(page: params[:position_page], per_page: 7)
     @prod = Product.all
+    @states = State.all.paginate(page: params[:state_page], per_page:11)
+    @employee_types = EmployeeType.all.paginate(page: params[:employee_type_page],per_page: 11)
+
   end
 
   def show
