6e9bcc7f7217f24b2ee03d2523c6a5e28804d80d
Luke Nguyen <Pho@Lukes-MacBook-Pro.local>
2017-04-10 16:47:04 -0500

    More styling changes

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,15 +2,31 @@ class AdminController < ApplicationController
   require 'will_paginate/array'
   def index
     @colors = Color.all.paginate(page: params[:color_page], per_page: 10)
-    @color = Color.new
     @products = Product.all.paginate(page: params[:product_page], per_page: 10)
+    @product_types = ProductType.all.paginate(page:params[:product_type_page], per_page:10)
     @event_types = EventType.all.paginate(page: params[:event_type_page], per_page: 10)
     @positions = Position.all.paginate(page: params[:position_page], per_page: 10)
-    @prod = Product.all
-    @states = State.all.paginate(page: params[:state_page], per_page:10)
     @employee_types = EmployeeType.all.paginate(page: params[:employee_type_page],per_page: 10)
+    @states = State.all.paginate(page: params[:state_page], per_page:10)
     @countries = Country.all.paginate(page: params[:country_page], per_page:10)
-    @product_types = ProductType.all.paginate(page:params[:product_type_page], per_page:10)
+
+
+    @color = Color.new
+    @product = Product.new
+    @product_type = ProductType.new
+    @event_type = EventType.new
+    @position = Position.new
+    @employee = Employee.new
+    @employee_status = EmployeeStatus.new
+    @employee_type = EmployeeType.new
+    @state = State.new
+    @country = Country.new
+    @customer_status = CustomerStatus.new
+  end
+
+  def new
+
+
 
   end
 
