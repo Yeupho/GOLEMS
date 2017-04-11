2d944cb51ebb1901e5b9946ffc822508b9ee88d1
Luke Nguyen <Pho@Lukes-MBP.attlocal.net>
2017-04-11 02:54:53 -0500

    Changes

--- app/controllers/pickups_controller.rb
+++ app/controllers/pickups_controller.rb
@@ -6,7 +6,29 @@ class PickupsController < ApplicationController
     @ready = Pickup.ready.paginate(page: params[:picked_up_page], per_page: 14)
   end
 
+
+  def update
+    respond_to do |format|
+      if CustomerEventProduct.update(pickups_params)
+        format.html { redirect_to '/pickups', notice: 'Customer event product was successfully updated.' }
+        format.json { head :no_content}
+      else
+        format.html { render :edit }
+        format.json { render json: @customer_event_product.errors, status: :unprocessable_entity }
+      end
+    end
+  end
   def progress
-    Pickup.progress.update_all({progress: true },{id: params[:pickup_status_id]})
+    CustomerEventProduct.update_all(['pickup_status_id', 2],{id: params[:pickup_status_ids]})
+    redirect_to pickups_url
+  end
+
+  private
+  def set_pickups
+    @customer_event_product = CustomerEventProduct.find(params[:id])
+  end
+
+  def pickups_params
+    params.require(:customer_event_product).permit(:customer_event_id, :product_id, :quantity, :pickup_status_id)
   end
 end
